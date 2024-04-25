b10f4f13421b0d1d2681b2baf172a553ae8553df
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-04-25 22:28:37 +0200

    With properly working test

--- src/test/java/net/ericwubbo/unittestingdemo/MessageServiceTest.java
+++ src/test/java/net/ericwubbo/unittestingdemo/MessageServiceTest.java
@@ -1,9 +1,12 @@
 package net.ericwubbo.unittestingdemo;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
+
+// @RequiredArgsConstructor does NOT work here, "No ParameterResolver registered for parameter" need @Autowired
 
 @SpringBootTest
 public class MessageServiceTest {
@@ -15,7 +18,7 @@ public class MessageServiceTest {
     public void titleizeShouldCapitalizeCorrectly() {
         // ARRANGE
         var input = "core java";
-        var expectedOutput = "core java";
+        var expectedOutput = "Core Java";
 
         // ACT
         var actualOutput = messageService.titleize(input);

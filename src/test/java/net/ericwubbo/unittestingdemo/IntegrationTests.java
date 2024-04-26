4c88cd19226fc7bc3c957fb8c03d3244cc42db55
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-04-26 09:05:43 +0200

    Removed extraneous imports

--- src/test/java/net/ericwubbo/unittestingdemo/IntegrationTests.java
+++ src/test/java/net/ericwubbo/unittestingdemo/IntegrationTests.java
@@ -3,18 +3,13 @@ package net.ericwubbo.unittestingdemo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
